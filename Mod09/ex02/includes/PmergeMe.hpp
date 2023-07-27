419d816876ff1a25fe37681e20fb4ec0499e67a3
Arakinos <you@example.com>
2023-07-27 18:58:03 +0200

    Update

--- Mod09/ex02/includes/PmergeMe.hpp
+++ Mod09/ex02/includes/PmergeMe.hpp
@@ -6,7 +6,7 @@
 /*   By: mjuin <mjuin@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/19 10:29:31 by mjuin             #+#    #+#             */
-/*   Updated: 2023/07/27 12:21:58 by mjuin            ###   ########.fr       */
+/*   Updated: 2023/07/27 18:09:39 by mjuin            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,28 +28,35 @@ class PmergeMe
 		PmergeMe(const PmergeMe&src);
 		~PmergeMe();
 		PmergeMe &operator=(const PmergeMe&);
-	private:
 		class Pair
 		{
 			private:
-				Pair(const Pair&src);
-				Pair &operator=(const Pair&);
+				Pair();
 				long int _min;
 				long int _max;
 				unsigned int _index;
 			public:
 				~Pair();
+				Pair(const Pair &src);
+				Pair &operator=(const Pair &src);
 				Pair(int min, int max, size_t index);
 				long int getMax();
 				long int getMin();
 				unsigned int getIndex();
-				void setIndex();
+				void setIndex(size_t index);
 		};
+	private:
 		PmergeMe();
-		float sortDeque();
-		float sortVector();
-		void sortPairMax(std::deque<Pair> pairdeque);
-		void printValue(std::string message);
+		long sortDeque();
+		long sortVector();
+		void sortPairMax(std::deque<Pair> *pairdeque, size_t size);
+		void sortPairMax(std::vector<Pair> *pairdeque, size_t size);
+		void printValue(std::string message, int Container, bool all);
+		void insertSort(std::deque<Pair> pairdeque);
+		void insertSort(std::vector<Pair> pairvector);
+		void BinaryInsertDeque(unsigned int element, unsigned int min, unsigned int max);
+		void BinaryInsertVector(unsigned int element, unsigned int min, unsigned int max);
+		void Verify();
 		std::deque<unsigned int> _deque;
 		std::vector<unsigned int> _vector;
 };
--- Mod09/ex02/srcs/PmergeMe.cpp
+++ Mod09/ex02/srcs/PmergeMe.cpp
@@ -1,55 +1,215 @@
-#include "../includes/PmergeMe.hpp"
+
 #include "PmergeMe.hpp"
+#include <ctime>
+#include <sys/time.h>
 
-PmergeMe::PmergeMe()
+/******************************************************************************************/
+/*                                                                                        */
+/*                                         Vector                                         */
+/*                                                                                        */
+/******************************************************************************************/
+
+long PmergeMe::sortVector()
 {
-
+    struct timeval time_now;
+    gettimeofday(&time_now, NULL);
+    time_t msecs_time = (time_now.tv_sec * 1000) + (time_now.tv_usec / 1000);
+	std::vector<Pair> pairvector;
+	int last = -1;
+	if (this->_vector.size() % 2 != 0)
+	{
+		last = this->_vector[this->_vector.size() - 1];
+		_vector.pop_back();
+	}
+	for (size_t i = 0; i < this->_vector.size(); i += 2)
+		pairvector.push_back(Pair(this->_vector[i], this->_vector[i + 1], i / 2));
+	sortPairMax(&pairvector, pairvector.size());
+	_vector.clear();
+	for (size_t i = 0; i < pairvector.size(); i++)
+		_vector.push_back(pairvector[i].getMax());
+	insertSort(pairvector);
+	if (last != -1)
+		BinaryInsertVector(last, 0, _vector.size());
+    struct timeval time_now2;
+    gettimeofday(&time_now2, NULL);
+    time_t msecs_time2 = (time_now2.tv_sec * 1000) + (time_now2.tv_usec / 1000);
+	return (msecs_time2 - msecs_time);
 }
 
-
-
-float PmergeMe::sortDeque()
+void PmergeMe::sortPairMax(std::vector<Pair> *pairvector, size_t size)
 {
-	clock_t startTV = clock();
+	if (size <= 1)
+		return;
+	sortPairMax(pairvector, size - 1);
+	int pos;
+	Pair value = (*pairvector)[size - 1];
+	for (pos = size - 2; pos >= 0 && ((*pairvector)[pos]).getMax() > value.getMax(); pos--)
+	{
+		(*pairvector)[pos + 1] = (*pairvector)[pos];
+		(*pairvector)[pos + 1].setIndex((pos + 1));
+	}
+	(*pairvector)[pos + 1] = value;
+	(*pairvector)[pos + 1].setIndex((pos + 1));
+}
+
+void PmergeMe::insertSort(std::vector<Pair> pairvector)
+{
+	_vector.insert(_vector.begin(), pairvector[0].getMin());
+	for (size_t y = 0; y < pairvector.size(); y++)
+	{
+		pairvector[y].setIndex(pairvector[y].getIndex() + 1);
+	}
+	unsigned int sequencePrevious1 = 0;
+	unsigned int sequencePrevious2 = 0;
+	unsigned int sequenceSize = 2;
+	size_t ElementNumber = pairvector.size() - 1;
+	unsigned int cur_element = 0;
+	while (cur_element != ElementNumber)
+	{
+		for (size_t i = 0; i < sequenceSize && cur_element < ElementNumber; i++)
+		{
+			BinaryInsertVector(pairvector[cur_element + sequenceSize - i].getMin(), 0, (pairvector[cur_element + sequenceSize - i].getIndex()));
+			for (size_t y = cur_element; y < pairvector.size(); y++)
+			{
+				if (pairvector[cur_element + sequenceSize - i].getMin() <= pairvector[y].getMax())
+					pairvector[y].setIndex(pairvector[y].getIndex() + 1);
+			}
+		}
+		cur_element += sequenceSize;
+		sequencePrevious1 = sequencePrevious2;
+		sequencePrevious2 = sequenceSize;
+		sequenceSize = sequencePrevious2 + (2 * sequencePrevious1);
+		if (cur_element + sequenceSize > ElementNumber)
+			sequenceSize = ElementNumber - cur_element;
+	}
+}
+
+void PmergeMe::BinaryInsertVector(unsigned int element, unsigned int min, unsigned int max)
+{
+	if (min + 1 == max)
+	{
+		if (_vector[min] < element && _vector[max] >= element)
+			_vector.insert(_vector.begin() + (min + 1), element);
+		else
+			_vector.insert(_vector.begin() + (min), element);
+	}
+	else if (min == max)
+		_vector.insert(_vector.begin() + (min + 1), element);
+	else
+	{
+		if (_vector[min + ((max - min) / 2)] > element)
+			BinaryInsertVector(element, min, max - ((max - min) / 2));
+		else
+			BinaryInsertVector(element, min + ((max - min) / 2), max);
+	}
+}
+
+/******************************************************************************************/
+/*                                                                                        */
+/*                                         Deque                                          */
+/*                                                                                        */
+/******************************************************************************************/
+
+long PmergeMe::sortDeque()
+{
+    struct timeval time_now;
+    gettimeofday(&time_now, NULL);
+    time_t msecs_time = (time_now.tv_sec * 1000) + (time_now.tv_usec / 1000);
 	std::deque<Pair> pairdeque;
 	int last = -1;
 	if (this->_deque.size() % 2 != 0)
+	{
 		last = this->_deque[this->_deque.size() - 1];
+		_deque.pop_back();
+	}
 	for (size_t i = 0; i < this->_deque.size(); i += 2)
 		pairdeque.push_back(Pair(this->_deque[i], this->_deque[i + 1], i / 2));
-	sortPairMax(pairdeque);
-	return ((float)(clock() - startTV)/CLOCKS_PER_SEC);
+	sortPairMax(&pairdeque, pairdeque.size());
+	_deque.clear();
+	for (size_t i = 0; i < pairdeque.size(); i++)
+		_deque.push_back(pairdeque[i].getMax());
+	insertSort(pairdeque);
+	if (last != -1)
+		BinaryInsertDeque(last, 0, _deque.size());
+    struct timeval time_now2;
+    gettimeofday(&time_now2, NULL);
+    time_t msecs_time2 = (time_now2.tv_sec * 1000) + (time_now2.tv_usec / 1000);
+	return (msecs_time2 - msecs_time);
 }
 
-float PmergeMe::sortVector()
-{
-	clock_t startTV = clock();
-
-	return ((float)(clock() - startTV)/CLOCKS_PER_SEC);
-}
-
-void PmergeMe::sortPairMax(std::deque<Pair> pairdeque, size_t size)
+void PmergeMe::sortPairMax(std::deque<Pair> *pairdeque, size_t size)
 {
 	if (size <= 1)
-		return ;
+		return;
+	sortPairMax(pairdeque, size - 1);
 	int pos;
-
-	for (pos = size - 2; pos >= 0 && ((*pairdeque)[pos]).getMin() > nl; pos--)
+	Pair value = (*pairdeque)[size - 1];
+	for (pos = size - 2; pos >= 0 && ((*pairdeque)[pos]).getMax() > value.getMax(); pos--)
 	{
 		(*pairdeque)[pos + 1] = (*pairdeque)[pos];
-		(*pairdeque)[pos + 1]
+		(*pairdeque)[pos + 1].setIndex((pos + 1));
 	}
-	
+	(*pairdeque)[pos + 1] = value;
+	(*pairdeque)[pos + 1].setIndex((pos + 1));
 }
 
-void PmergeMe::printValue(std::string message)
+void PmergeMe::insertSort(std::deque<Pair> pairdeque)
 {
-	std::vector<unsigned int>::iterator it2 = _vector.end();
-	std::cout << message;
-	for (std::vector<unsigned int>::iterator it = _vector.begin() ; it != it2; it++)
-		std::cout << " " << *it;
-	std::cout << std::endl;
+	_deque.push_front(pairdeque[0].getMin());
+	for (size_t y = 0; y < pairdeque.size(); y++)
+	{
+			pairdeque[y].setIndex(pairdeque[y].getIndex() + 1);
+	}
+	unsigned int sequencePrevious1 = 0;
+	unsigned int sequencePrevious2 = 0;
+	unsigned int sequenceSize = 2;
+	size_t ElementNumber = pairdeque.size() - 1;
+	unsigned int cur_element = 0;
+	while (cur_element != ElementNumber)
+	{
+		for (size_t i = 0; i < sequenceSize && cur_element < ElementNumber; i++)
+		{
+			BinaryInsertDeque(pairdeque[cur_element + sequenceSize - i].getMin(), 0, (pairdeque[cur_element + sequenceSize - i].getIndex()));
+			for (size_t y = cur_element; y < pairdeque.size(); y++)
+			{
+				if (pairdeque[cur_element + sequenceSize - i].getMin() <= pairdeque[y].getMax())
+					pairdeque[y].setIndex(pairdeque[y].getIndex() + 1);
+			}
+		}
+		cur_element += sequenceSize;
+		sequencePrevious1 = sequencePrevious2;
+		sequencePrevious2 = sequenceSize;
+		sequenceSize = sequencePrevious2 + (2 * sequencePrevious1);
+		if (cur_element + sequenceSize > ElementNumber)
+			sequenceSize = ElementNumber - cur_element;
+	}
 }
+
+void PmergeMe::BinaryInsertDeque(unsigned int element, unsigned int min, unsigned int max)
+{
+	if (min + 1 == max)
+	{
+		if (_deque[min] < element && _deque[max] >= element)
+			_deque.insert(_deque.begin() + (min + 1), element);
+		else
+			_deque.insert(_deque.begin() + (min), element);
+	}
+	else if (min == max)
+		_deque.insert(_deque.begin() + (min + 1), element);
+	else
+	{
+		if (_deque[min + ((max - min) / 2)] > element)
+			BinaryInsertDeque(element, min, max - ((max - min) / 2));
+		else
+			BinaryInsertDeque(element, min + ((max - min) / 2), max);
+	}
+}
+
+/******************************************************************************************/
+/*                                                                                        */
+/*                                         Common                                         */
+/*                                                                                        */
+/******************************************************************************************/
 
 PmergeMe::PmergeMe(int arg_count, char **av)
 {
@@ -75,21 +235,112 @@ PmergeMe::PmergeMe(int arg_count, char **av)
 		_deque.push_back(atoied);
 		_vector.push_back(atoied);
 	}
-	printValue("Before =");
+	printValue("Before =", 0, false);
 	float t_vector = sortVector();
 	float t_deque = sortDeque();
-	printValue("After =");
-	std::cout << "Time to process a range of " << _deque.size() << " elements with std::deque : " << std::fixed << t_deque << " s\n";
-	std::cout << "Time to process a range of " << _vector.size() << " elements with std::vector : " << std::fixed << t_vector << " s\n";
+	printValue("After =", 0, false);
+	std::cout << "Time to process a range of " << _deque.size() << " elements with std::deque : " << t_deque << " ms\n";
+	std::cout << "Time to process a range of " << _vector.size() << " elements with std::vector : " << t_vector << " ms\n";
+	//Verify();
 }
+
+void PmergeMe::printValue(std::string message, int Container = 0, bool all = true)
+{
+	if (Container != 0 && Container != 1)
+		return;
+	if (Container == 0)
+	{
+
+		std::vector<unsigned int>::iterator it2 = (all == false && _vector.size() >= 4) ? _vector.begin() += 4 : _vector.end();
+		std::cout << message;
+		std::vector<unsigned int>::iterator it;
+		for (it = _vector.begin(); it != it2; it++)
+			std::cout << " " << *it;
+		if (it != _vector.end())
+			std::cout << " [...]";
+		std::cout << std::endl;
+	}
+	else
+	{
+		std::deque<unsigned int>::iterator it2 = (all == false && _deque.size() >= 4) ? _deque.begin() += 4 : _deque.end();
+		std::cout << message;
+		std::deque<unsigned int>::iterator it;
+		for (it = _deque.begin(); it != it2; it++)
+			std::cout << " " << *it;
+		if (it != _deque.end())
+			std::cout << " [...]";
+		std::cout << std::endl;
+	}
+}
+
+void PmergeMe::Verify()
+{
+	unsigned int previous = 0;
+	for (size_t i = 0; i < _deque.size(); i++)
+	{
+		if (i == 0)
+		{
+			previous = _deque[i];
+			continue;
+		}
+		if (previous <= _deque[i])
+		{
+			previous = _deque[i];
+			continue;
+		}
+		else
+		{
+			std::cout << "\033[1;31m" << "Error in Element " << i << "of deque" << "\033[0m" << std::endl;
+			return ;
+		}
+	}
+	for (size_t i = 0; i < _vector.size(); i++)
+	{
+		if (i == 0)
+		{
+			previous = _vector[i];
+			continue;
+		}
+		if (previous <= _vector[i])
+		{
+			previous = _vector[i];
+			continue;
+		}
+		else
+		{
+			std::cout << "\033[1;31m" << "Error in Element " << i << "of vector" << "\033[0m" << std::endl;
+			return ;
+		}
+	}
+	std::cout << "\033[1;32m" << "All si good for both container" << "\033[0m" << std::endl;
+}
+
+/******************************************************************************************/
+/*                                                                                        */
+/*                                   Custom Constructor                                   */
+/*                                                                                        */
+/******************************************************************************************/
+
+PmergeMe::Pair::Pair(int min, int max, size_t index)
+{
+	this->_min = (min < max) ? min : max;
+	this->_max = (min <= max) ? max : min;
+	this->_index = index;
+}
+
+/******************************************************************************************/
+/*                                                                                        */
+/*                                     Canonical Form                                     */
+/*                                                                                        */
+/******************************************************************************************/
+
+PmergeMe::PmergeMe() {}
+
+PmergeMe::~PmergeMe() {}
 
 PmergeMe::PmergeMe(const PmergeMe &src)
 {
 	*this = src;
-}
-
-PmergeMe::~PmergeMe()
-{
 }
 
 PmergeMe &PmergeMe::operator=(const PmergeMe &src)
@@ -101,23 +352,47 @@ PmergeMe &PmergeMe::operator=(const PmergeMe &src)
 	return (*this);
 }
 
-PmergeMe::Pair::Pair(int min, int max, size_t index)
+PmergeMe::Pair::Pair() {}
+
+PmergeMe::Pair::~Pair() {}
+
+PmergeMe::Pair::Pair(const Pair &src)
 {
-	this->_min = (min < max) ? min : max;
-	this->_max = (min <= max) ? max : min;
-	this->_index = index;
+	*this = src;
 }
+
+PmergeMe::Pair &PmergeMe::Pair::operator=(const PmergeMe::Pair &src)
+{
+	if (this == &src)
+		return (*this);
+	this->_index = src._index;
+	this->_max = src._max;
+	this->_min = src._min;
+	return (*this);
+}
+
+/******************************************************************************************/
+/*                                                                                        */
+/*                                     Getter / Setter                                    */
+/*                                                                                        */
+/******************************************************************************************/
 
 long int PmergeMe::Pair::getMax()
 {
-    return this->_max;
+	return this->_max;
 }
 
 long int PmergeMe::Pair::getMin()
 {
-    return this->_min;
+	return this->_min;
 }
 
-void PmergeMe::Pair::setIndex()
+unsigned int PmergeMe::Pair::getIndex()
 {
+	return this->_index;
+}
+
+void PmergeMe::Pair::setIndex(size_t index)
+{
+	this->_index = index;
 }
